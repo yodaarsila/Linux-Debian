0d32a4e122bbab1c92a6992fd2dc4c2b324bf01b
Yoda Arsila <156045219+yodaarsila@users.noreply.github.com>
2024-05-03 22:34:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm ../usr/bin/Install*
-wget https://raw.githubusercontent.com/wahasa/Debian/main/Install/Install -P ../usr/bin/
+wget https://raw.githubusercontent.com/yodaarsila/Debian/main/Install/Install -P ../usr/bin/
 chmod +x ../usr/bin/Install
   echo ""
   echo "Installation successful,."
